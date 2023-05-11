2dfc27f36e0dd6f56715521ec5a9392fee79355f
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-11 22:20:38 +0900

    구현; 소개팅_ver3
    - 정렬 후, 단순히 양 끝을 비교하는 것으로 타임아웃 문제 해결
    - 한쪽이 더 커서 한쪽의 인덱스만 2번 옮기게 될 경우, 마지막의 가운데 값을 확인 안함. 절반보다 1번더 반복해야함.
    - 해설 : dict or abs() 사용도 가능.
    - BUT, 그냥 문제를 잘 이해한 0으로 만들어준 답이 최고..

--- python_algorithm/Implementation/0_couple.py
+++ python_algorithm/Implementation/0_couple.py
@@ -1,19 +1,31 @@
 # 출처 : 멀티잇 코딩테스트 러닝클래스'Python 5월반
 # 시뮬레이션과 창의적 해결법 - 소개팅해주기
-# 2차시도
+# 3차시도 : 단순히 리스트의 양 끝단에서 비교하는 로직으로 수정!
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-s_list = list(map(int, input().split()))
-s_list.sort()           #점수를 정렬해서
-answer = s_list.copy()
-for s in s_list[:n//2]: #절반만 확인
-	if s in answer and -s in answer: #answer에 둘다 있으면 제거
-		answer.remove(s)
-		answer.remove(-s)
-print(sum(answer)) #남은 정답의 합계 구했는데... timeout.
+s = list(map(int, input().split()))
+s.sort()
+answer = 0
+left = 0   #음수
+right = n-1  #양수
 
-# 전체를 다 안 돌고 절반만 돌렸는데도 시간초과라는 건,
-# 복사를 하고 삭제를 하는 과정 자체가 시간이 많이 걸리는 걸까?
-# 그렇다면 제거를 하는게 아니라 양쪽 끝에서 0점을 계산하는 방식으로 가야하는 건가?
+# 처음엔 n//2로 했다가 fail.
+# 값이 한쪽으로 변향되게 더 많은 경우는 정가운데 값이 확인이 아는 경우가 있음
+for _ in range(n//2+1):      # n//2 +1 로 한번더 검사해야하는 것이 핵심
+	if -s[left] == s[right]: # 양끝이 동일하면 pass
+		left += 1
+		right -= 1
+	elif -s[left] > s[right]: # 음수가 더 크면 음수만 위치 변경해서 양수값과 맞춰주기
+		answer += s[left]
+		left += 1
+	elif -s[left] < s[right]: # 양수가 더 크면, 양수 위치 변경
+		answer += s[right]
+		right -= 1
+	else: pass
+print(answer)
+
+#  정답 1: hash 기반 - dict에 한쪽 기록하고 없는 값만 추가
+#  정답 2: 절대값 개수 이용 - 절대값 개수를 센 후 절대값이 1번만 등장한 경우 정답.
+#  정닶 3: n-n=0, 고로 전체 더하면 정답!! 와.... 정말 대박인 문제...ㄷㄷㄷㄷ
