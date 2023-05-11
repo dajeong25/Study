2e51f6ce96b0e3499e3a481fa1322668f6ac8a75
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-11 21:52:25 +0900

    구현; 소개팅해주기_ver2
    - 타임아웃을 개선하고자 절반만 확인 하는 걸 로직을 바꿨지만 여전히 동인하게 시간초과남

--- python_algorithm/Implementation/0_couple.py
+++ python_algorithm/Implementation/0_couple.py
@@ -1,13 +1,19 @@
 # 출처 : 멀티잇 코딩테스트 러닝클래스'Python 5월반
 # 시뮬레이션과 창의적 해결법 - 소개팅해주기
+# 2차시도
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 s_list = list(map(int, input().split()))
-answer = 0
-for s in s_list:
-	if not -s in s_list:
-		answer += s
-print(answer) #timeout...
+s_list.sort()           #점수를 정렬해서
+answer = s_list.copy()
+for s in s_list[:n//2]: #절반만 확인
+	if s in answer and -s in answer: #answer에 둘다 있으면 제거
+		answer.remove(s)
+		answer.remove(-s)
+print(sum(answer)) #남은 정답의 합계 구했는데... timeout.
 
+# 전체를 다 안 돌고 절반만 돌렸는데도 시간초과라는 건,
+# 복사를 하고 삭제를 하는 과정 자체가 시간이 많이 걸리는 걸까?
+# 그렇다면 제거를 하는게 아니라 양쪽 끝에서 0점을 계산하는 방식으로 가야하는 건가?
