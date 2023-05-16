933f4280675e7fd416758446234d7bc0bbdbe367
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-16 15:43:13 +0900

    완전탐색; 맨하튼 ver2
    - 경우의 수가 많아서 모름모꼴 탐색 pass. 가장 처음 생각했던 좌표를 가져와서 전체 계산하는 걸로 했지만 fail.
    안되는 경우가 어떤게 있을 지 고민해서 알고리즘 수정 필요

--- python_algorithm/search/full_search_manhattan.py
+++ python_algorithm/search/full_search_manhattan.py
@@ -1,31 +1,25 @@
 # 출처 : 멀티잇 코딩테스트 러닝클래스'Python 5월반
 # 완전탐색 - 맨하튼거리
+# 1차 - 모름로꼴 전체 탐색 > 경우의 수가 많아서 pass
+# 2차 - 좌표를 가져와서 맨하튼 거리 측정
 import sys
 input = sys.stdin.readline
-
 N, M = map(int, input().split())
-ground = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
-# 수색범위는 마름모모양 O(M^2)
-# 시간복잡도 O(N^2 M^2)
+ants, foods = [], []
+for y in range(N):
+	ground = list(map(int, input().rstrip().split()))
+	for x in range(N):
+		if ground[x] == 1:
+			ants.append([y,x])
+		elif ground[x] == 2:
+			foods.append([y,x])
+		else:pass
 
 answer = 0
-for y in range(N):
-	for x in range(N):
-		if ground[y][x] == 1:
-			print('---', y, x, '----')
-			for ny in range(1, M+1):
-				for nx in range(1, M+1):
-					dy = [-ny, 0, 0, ny]
-					dx = [0, -nx, nx, 0]
-					
-					for i in range(4):
-						my = y + dy[i]
-						mx = x + dx[i]
-						if my<0 or my>=N or mx<0 or mx>=N:
-							continue
-						print(ny, nx, my, mx, ground[my][mx])
-						if ground[my][mx] == 2:
-							answer += 1
+for y1, x1 in ants:
+	for y2, x2 in foods:
+		if (abs(y1-y2) + abs(x1-x2)) <= M:
+			answer += 1
 
-print(answer//2)
+print(answer) #fail..
