fa84df86f2cdff402498af506e159663d13a5864
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-16 10:51:21 +0900

    구현; 폭탄 ver2
    - 좌표를 이동할 때는 for문으로 이동하면서 해야 에러가 안 나오고 효율적임.
    - 나열식이 아닌 자동화를 생각합시다!

--- python_algorithm/Implementation/bomb.py
+++ python_algorithm/Implementation/bomb.py
@@ -1,21 +1,23 @@
 ## 다시 하기(현재 코드는 숫자부분에 에러 발생함 수정 필요)
 # 출처 : 멀티잇 코딩테스트 러닝클래스'Python 5월반
 # 시뮬레이션과 창의적 해결법 - 폭탄 구현
-
-n, k = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split()) # 각각의 원소로 받을 때는 list 필요 없음
+	# 위, 왼, 오, 아래
+dx = [0, -1, 1, 0] 
+dy = [-1, 0, 0, 1]
 
 ground = [[0 for i in range(n)] for j in range(n)]
 for _ in range(k):
-	y, x = list(map(int, input().split()))
+	y, x = map(int, input().split()) # 각각의 원소로 받을 때는 list 필요 없음
 	y, x = y-1, x-1
 	ground[y][x] += 1
-	if x-1>=0 :
-		ground[y][x-1] += 1
-	if x+1<k:
-		ground[y][x+1] += 1
-	if y-1>=0:
-		ground[y-1][x] += 1
-	if y+1<k:
-		ground[y+1][x] += 1
+	for i in range(4): # 이전의 4개의 if문 아닌 좌표 위치를 받고 이동하면서 더해야 에러가 없음 + 효율적
+		ny = y + dy[i]
+		nx = x + dx[i]
+		if ny<0 or ny>=n or nx<0 or nx>=n:
+			continue
+		ground[ny][nx] += 1
 
 print(sum(sum(x) for x in ground))
