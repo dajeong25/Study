268af3dc526b071f280a33bedc8e86ccc610d094
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-20 23:41:22 +0900

    완전탐색; 맨하튼 ver3
    - for문에 break 안하는 바람에 중복해서 개미의 수를 세어 오류 남.
    - 문제에서 요구하는 것이 무엇인지 정확하게 아는 것이 중요!

--- python_algorithm/search/full_search_manhattan.py
+++ python_algorithm/search/full_search_manhattan.py
@@ -2,6 +2,10 @@
 # 완전탐색 - 맨하튼거리
 # 1차 - 모름로꼴 전체 탐색 > 경우의 수가 많아서 pass
 # 2차 - 좌표를 가져와서 맨하튼 거리 측정
+# 3차 - 문제를 제대로 읽지 않은 자의 최후.. 
+# 	>> 개미가 진딧물을 얻을 수 있으면 ok. 
+# 		그 개미가 몇개의 진딧물을 받을 수 있는 지는 상관없음.
+
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
@@ -21,5 +25,6 @@ for y1, x1 in ants:
 	for y2, x2 in foods:
 		if (abs(y1-y2) + abs(x1-x2)) <= M:
 			answer += 1
+			break # 이걸 추가해야 개미 수가 중복 안됨!!
 
-print(answer) #fail..
+print(answer)
