ddcba67aede2dc5d33104a361c0e5b87c67f1975
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-26 23:45:39 +0900

    그래프; 물체탐색 ver2
    - 가장 큰 물체 크기를 값을 잡았지만 에러

--- python_algorithm/graph/bfs_object.py
+++ python_algorithm/graph/bfs_object.py
@@ -1,6 +1,7 @@
 # 출처 : 멀티잇 코딩테스트 러닝클래스'Python 5월반
 # 그래프 탐색 - 물체 탐색 (사후)
-# 1차 시도 - 3개 통과되고 실패. 해설도 분명히 봤는데 뭘 놓친 걸까?
+# 1차시도 - 3개 통과되고 실패.
+# 2차시도 - max 값이 무한히 늘어나는 코드를 수정했지만 대다수 에러
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -20,6 +21,7 @@ for y in range(M):
 		if obj[y][x] == '#' and visited[y][x] != 1:
 			visited[y][x] = 1
 			cnt += 1
+			temp_cnt = 1
 			size = deque()
 			size.append((y,x))
 			while size:
@@ -30,10 +32,10 @@ for y in range(M):
 					if ny<0 or ny>=M or nx<0 or nx>=N :
 						continue
 					if obj[ny][nx] == '#' and visited[ny][nx] != 1:
-						max_cnt += 1
+						temp_cnt += 1
 						size.append((ny,nx))
-					visited[ny][nx] = 1
+						visited[ny][nx] = 1
+			if temp_cnt > max_cnt:
+				max_cnt = temp_cnt
 print(cnt)
 print(max_cnt)
-
-
