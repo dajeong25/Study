75ed3a9bbbb5d0054f1f534a6ed642eca3dcb6ea
DESKTOP-DBNQ0KF\user <hdj1196@gmail.com>
2023-05-27 23:23:50 +0900

    [다시] 그래프; 물체탐색ver3
    - 아무리 수정해도 모르겠다. 뭐가 문제인가.
    - 해설을 그대로 붙여넣어도 시간초과 떠서 더욱 모르겠음 -> 도움 요청 완료

--- python_algorithm/graph/bfs_object.py
+++ python_algorithm/graph/bfs_object.py
@@ -7,35 +7,69 @@ from collections import deque
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-obj = [input().strip() for _ in range(M)]
+p = [input().rstrip() for _ in range(M)]
 
 # 상 하 좌 우
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
 
 cnt = 0
-max_cnt = 1
+max_size = 0
 visited = [[0 for _ in range(N)] for _ in range(M)]
 for y in range(M):
 	for x in range(N):
-		if obj[y][x] == '#' and visited[y][x] != 1:
+		if p[y][x] == '#' and visited[y][x] == 0:
 			visited[y][x] = 1
 			cnt += 1
-			temp_cnt = 1
-			size = deque()
-			size.append((y,x))
-			while size:
-				y, x = size.popleft()
+			temp = 0
+			s = deque()
+			s.append((y,x))
+			while s:
+				y, x = s.popleft()
+				temp += 1
 				for i in range(4):
 					ny = dy[i] + y
 					nx = dx[i] + x
-					if ny<0 or ny>=M or nx<0 or nx>=N :
+					if ny<0 or ny>=M or nx<0 or nx>=N or p[ny][nx] == '.' or visited[ny][nx] == 1:
 						continue
-					if obj[ny][nx] == '#' and visited[ny][nx] != 1:
-						temp_cnt += 1
-						size.append((ny,nx))
-						visited[ny][nx] = 1
-			if temp_cnt > max_cnt:
-				max_cnt = temp_cnt
+					s.append((ny,nx))
+					visited[ny][nx] = 1
+			max_size = max(temp, max_size)
+
 print(cnt)
-print(max_cnt)
+print(max_size)
+#아무리 봐도 어디가 에러인지 모르겠다!! 근데 해설을 그대로 붙여넣어도 timeout이 나서 실패하는데...?????
+
+#해설
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+S = [input().rstrip() for _ in range(M)]
+V = [[0 for _ in range(N)] for _ in range(M)]
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+component, ans = 0, 0
+for i in range(M):
+    for j in range(N):
+        if V[i][j] or S[i][j] == '.':
+            continue
+        cnt = 0
+        component += 1
+        V[i][j] = 1
+        Q = deque()
+        Q.append((i, j))
+        while Q:
+            y, x = Q.popleft()
+            cnt += 1
+            for k in range(4):
+                ny, nx = y + dy[k], x + dx[k]
+                if ny < 0 or nx < 0 or ny >= M or nx >= N or V[ny][nx] or S[ny][nx] == '.':
+                    continue
+                V[ny][nx] = 1
+                Q.append((ny, nx))
+        ans = max(ans, cnt)
+
+print(component, ans, sep='\n')
